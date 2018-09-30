a0716b7177463b4015315401bd3ae645a949ddaa
Hermes <hermesrt@gmail.com>
2018-09-30 14:48:38 -0300

    commit incial

---
+++ application/controllers/C_Inicio.php
@@ -0,0 +1,16 @@
+
+<?php
+ defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+ class C_Inicio extends CI_Controller {
+ 
+ 	public function index()
+ 	{
+ 		$this->load->view('Plantillas/V_Header');		
+ 		$this->load->view('V_Inicio');		
+ 		$this->load->view('Plantillas/V_Footer');		
+ 	}
+ 
+ }
+
+ ?>
---
+++ application/views/Plantillas/V_Footer.php
@@ -0,0 +1,9 @@
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="<?= base_url('assets/js/jquery-3.3.1.min.js') ?>"></script>
+    <script type="text/javascript" src="<?= base_url('assets/js/popper.min.js') ?>"></script>
+    <script type="text/javascript" src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+
+  </body>
+
+</html>
