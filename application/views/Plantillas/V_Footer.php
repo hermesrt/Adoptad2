ef6354c02347c358374886d4cc7e00fd7de8fbb1
Hermes <hermesrt@gmail.com>
2018-09-30 21:44:22 -0300

    Agregue Footer

--- application/views/Plantillas/V_Footer.php
+++ application/views/Plantillas/V_Footer.php
@@ -7,20 +7,20 @@
     <footer>
     	<div class="container-fluid text-light bg-dark">
     		<center><div class="row">
-    			<div class="col-4">
+    			<div class="col-4 my-2">
     				<h6><i class="fas fa-users"></i> Nosotros:</h6>
     				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
     			</div>	
-    			<div class="col-4">
-    				<h6>Desarrolladores:</h6>
+    			<div class="col-4 my-2">
+    				<h6><i class="fas fa-code"></i> Desarrolladores:</h6>
     				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
     			</div>	
-    			<div class="col-4">
-    				<h6>Avalado por:</h6>
+    			<div class="col-4 my-2">
+    				<h6><i class="fas fa-address-card"></i> Avalado por:</h6>
     				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
