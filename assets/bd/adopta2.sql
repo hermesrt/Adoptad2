f1737b30e9b4b0dcfadc500153f10beac2a0b0cf
lucas <lucasficus@gmail.com>
2018-10-02 17:42:09 -0300

    Subi base de datos
    subi la bd, una imagen de como quedo el esquema, y el diccionario de datos, para que sepan que tipo es cada dato y cuales son las claves. El borrado de las relaciones es en cascada y el update de las relaciones tambien es en cascada

---
+++ assets/bd/adopta2.sql
@@ -0,0 +1,380 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-10-2018 a las 22:28:09
+-- Versión del servidor: 10.1.9-MariaDB
+-- Versión de PHP: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `adopta2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `adopcion`
+--
+
+CREATE TABLE `adopcion` (
+  `id_adopcion` int(11) NOT NULL,
+  `fecha_adopcion` date NOT NULL,
+  `detalle_adopcion` text NOT NULL,
+  `id_adoptante` int(11) NOT NULL,
+  `id_animal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `adoptante`
+--
+
+CREATE TABLE `adoptante` (
+  `id_adoptante` int(11) NOT NULL,
+  `dni_adoptante` int(8) NOT NULL,
+  `nombre_adoptante` varchar(50) NOT NULL,
+  `apellido_adoptante` varchar(50) NOT NULL,
+  `direccion_adoptante` varchar(80) NOT NULL,
+  `telefono_adoptante` int(15) NOT NULL,
+  `email_adoptante` varchar(50) NOT NULL,
+  `ciudad_adoptante` varchar(50) NOT NULL,
+  `estado_adoptante` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `animal`
+--
+
+CREATE TABLE `animal` (
+  `id_animal` int(11) NOT NULL,
+  `nombre_animal` varchar(50) NOT NULL,
+  `raza_animal` varchar(50) NOT NULL,
+  `especie_animal` varchar(50) NOT NULL,
+  `sexo_animal` varchar(10) NOT NULL,
+  `edad_animal` varchar(10) NOT NULL,
+  `descripcion_animal` varchar(100) NOT NULL,
+  `estado_animal` int(1) NOT NULL,
+  `castrado` int(1) NOT NULL,
+  `nombre_imagen_animal` varchar(100) NOT NULL,
+  `id_centro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `centro_adopcion`
+--
+
+CREATE TABLE `centro_adopcion` (
+  `id_centro` int(11) NOT NULL,
+  `nombre_ca` varchar(50) NOT NULL,
+  `direccion_ca` varchar(80) NOT NULL,
+  `telefono_ca` int(15) NOT NULL,
+  `email_ca` varchar(50) NOT NULL,
+  `ciudad_ca` varchar(50) NOT NULL,
+  `estado_ca` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `denuncia`
+--
+
+CREATE TABLE `denuncia` (
+  `id_denuncia` int(11) NOT NULL,
+  `fecha_denuncia` date NOT NULL,
+  `id_adoptante` int(11) NOT NULL,
+  `id_motivo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `motivo_denuncia`
+--
+
+CREATE TABLE `motivo_denuncia` (
+  `id_motivo` int(11) NOT NULL,
+  `motivo_denuncia` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `periodo_seguimiento`
+--
+
+CREATE TABLE `periodo_seguimiento` (
+  `id_periodo` int(11) NOT NULL,
+  `tipo_periodo` varchar(50) NOT NULL,
+  `fecha_inicio_periodo` date NOT NULL,
+  `fecha_fin_periodo` date NOT NULL,
+  `id_centro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `revision`
+--
+
+CREATE TABLE `revision` (
+  `id_revision` int(11) NOT NULL,
+  `fecha_revision` date NOT NULL,
+  `tipo_revision` varchar(15) NOT NULL,
+  `detalle_revision` varchar(150) NOT NULL,
+  `id_animal` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `dni_usuario` int(8) NOT NULL,
+  `password` int(11) NOT NULL,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `apellido_usuarrio` varchar(50) NOT NULL,
+  `email_usuario` varchar(50) NOT NULL,
+  `telefono_usuario` int(15) NOT NULL,
+  `domicilio_usuario` varchar(50) NOT NULL,
+  `matricula` varchar(15) NOT NULL,
+  `tipo_usuario` varchar(15) NOT NULL,
+  `id_centro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vacuna`
+--
+
+CREATE TABLE `vacuna` (
+  `id_vacuna` int(11) NOT NULL,
+  `nombre_vacuna` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vacuna_aplicada`
+--
+
+CREATE TABLE `vacuna_aplicada` (
+  `id_vacuna_aplicada` int(11) NOT NULL,
+  `fecha_aplicacion_vacuna` date NOT NULL,
+  `id_animal` int(11) NOT NULL,
+  `id_vacuna` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `adopcion`
+--
+ALTER TABLE `adopcion`
+  ADD PRIMARY KEY (`id_adopcion`),
+  ADD KEY `id_adoptante` (`id_adoptante`,`id_animal`),
+  ADD KEY `id_animal` (`id_animal`);
+
+--
+-- Indices de la tabla `adoptante`
+--
+ALTER TABLE `adoptante`
+  ADD PRIMARY KEY (`id_adoptante`);
+
+--
+-- Indices de la tabla `animal`
+--
+ALTER TABLE `animal`
+  ADD PRIMARY KEY (`id_animal`),
+  ADD KEY `id_centro` (`id_centro`);
+
+--
+-- Indices de la tabla `centro_adopcion`
+--
+ALTER TABLE `centro_adopcion`
+  ADD PRIMARY KEY (`id_centro`);
+
+--
+-- Indices de la tabla `denuncia`
+--
+ALTER TABLE `denuncia`
+  ADD PRIMARY KEY (`id_denuncia`),
+  ADD KEY `id_adoptante` (`id_adoptante`,`id_motivo`),
+  ADD KEY `id_motivo` (`id_motivo`);
+
+--
+-- Indices de la tabla `motivo_denuncia`
+--
+ALTER TABLE `motivo_denuncia`
+  ADD PRIMARY KEY (`id_motivo`);
+
+--
+-- Indices de la tabla `periodo_seguimiento`
+--
+ALTER TABLE `periodo_seguimiento`
+  ADD PRIMARY KEY (`id_periodo`),
+  ADD KEY `id_centro` (`id_centro`);
+
+--
+-- Indices de la tabla `revision`
+--
+ALTER TABLE `revision`
+  ADD PRIMARY KEY (`id_revision`),
+  ADD KEY `id_animal` (`id_animal`,`id_usuario`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `id_centro` (`id_centro`);
+
+--
+-- Indices de la tabla `vacuna`
+--
+ALTER TABLE `vacuna`
+  ADD PRIMARY KEY (`id_vacuna`);
+
+--
+-- Indices de la tabla `vacuna_aplicada`
+--
+ALTER TABLE `vacuna_aplicada`
+  ADD PRIMARY KEY (`id_vacuna_aplicada`),
+  ADD KEY `id_animal` (`id_animal`,`id_vacuna`),
+  ADD KEY `id_vacuna` (`id_vacuna`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `adopcion`
+--
+ALTER TABLE `adopcion`
+  MODIFY `id_adopcion` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `adoptante`
+--
+ALTER TABLE `adoptante`
+  MODIFY `id_adoptante` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `animal`
+--
+ALTER TABLE `animal`
+  MODIFY `id_animal` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `centro_adopcion`
+--
+ALTER TABLE `centro_adopcion`
+  MODIFY `id_centro` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `denuncia`
+--
+ALTER TABLE `denuncia`
+  MODIFY `id_denuncia` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `motivo_denuncia`
+--
+ALTER TABLE `motivo_denuncia`
+  MODIFY `id_motivo` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `periodo_seguimiento`
+--
+ALTER TABLE `periodo_seguimiento`
+  MODIFY `id_periodo` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `revision`
+--
+ALTER TABLE `revision`
+  MODIFY `id_revision` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `vacuna`
+--
+ALTER TABLE `vacuna`
+  MODIFY `id_vacuna` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `vacuna_aplicada`
+--
+ALTER TABLE `vacuna_aplicada`
+  MODIFY `id_vacuna_aplicada` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `adopcion`
+--
+ALTER TABLE `adopcion`
+  ADD CONSTRAINT `adopcion_ibfk_1` FOREIGN KEY (`id_adoptante`) REFERENCES `adoptante` (`id_adoptante`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `adopcion_ibfk_2` FOREIGN KEY (`id_animal`) REFERENCES `animal` (`id_animal`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `animal`
+--
+ALTER TABLE `animal`
+  ADD CONSTRAINT `animal_ibfk_1` FOREIGN KEY (`id_centro`) REFERENCES `centro_adopcion` (`id_centro`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `denuncia`
+--
+ALTER TABLE `denuncia`
+  ADD CONSTRAINT `denuncia_ibfk_1` FOREIGN KEY (`id_motivo`) REFERENCES `motivo_denuncia` (`id_motivo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `denuncia_ibfk_2` FOREIGN KEY (`id_adoptante`) REFERENCES `adoptante` (`id_adoptante`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `periodo_seguimiento`
+--
+ALTER TABLE `periodo_seguimiento`
+  ADD CONSTRAINT `periodo_seguimiento_ibfk_1` FOREIGN KEY (`id_centro`) REFERENCES `centro_adopcion` (`id_centro`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `revision`
+--
+ALTER TABLE `revision`
+  ADD CONSTRAINT `revision_ibfk_1` FOREIGN KEY (`id_animal`) REFERENCES `animal` (`id_animal`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `revision_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_centro`) REFERENCES `centro_adopcion` (`id_centro`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `vacuna_aplicada`
+--
+ALTER TABLE `vacuna_aplicada`
+  ADD CONSTRAINT `vacuna_aplicada_ibfk_1` FOREIGN KEY (`id_animal`) REFERENCES `animal` (`id_animal`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `vacuna_aplicada_ibfk_2` FOREIGN KEY (`id_vacuna`) REFERENCES `vacuna` (`id_vacuna`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
