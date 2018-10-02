028875bf048c94fee39eba85d8c311fff02d4eee
Hermes <hermesrt@gmail.com>
2018-10-01 22:32:57 -0300

    Modificacion Vista Registrar Revision

--- application/views/V_NuevaRevision.php
+++ application/views/V_NuevaRevision.php
@@ -14,7 +14,7 @@
 						</ul>
 					</p>					
 					<div class="alert alert-warning" role="alert">
-						<p class="font-italic"> <i class="fas fa-info"></i> A izquierda puedes visualizar los animales adoptados por esta persona. Seleciona el boton "Registrar Revision" en que desees.</p>
+						<p class="font-italic"> <i class="fas fa-info"></i> En esta seccion puedes visualizar los animales adoptados por esta persona. Seleciona el boton "Registrar Revision" en que desees.</p>
 					</div>
 				</div>
 			</div>
@@ -41,8 +41,8 @@
 							</div>
 							<br>
 							<center><button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">
-  Registrar Revision
-</button></center>
+								Registrar Revision
+							</button></center>
 						</li>
 						<li class="list-group-item">
 							<div class="row">
@@ -60,8 +60,8 @@
 							</div>
 							<br>
 							<center><button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">
-  Registrar Revision
-</button></center>
+								Registrar Revision
+							</button></center>
 
 						</li>
 					</ul>
@@ -76,21 +76,58 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Nueva Revision</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary">Registrar Revision</button>
-      </div>
-    </div>
-  </div>
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Nueva Revision</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="form-group">
+					<label for="fecha">Fecha</label>
+					<input type="date" class="form-control" id="fecha" placeholder="Fecha">
+				</div>
+				<div class="form-group">
+					<label for="TipoRevision">Tipo de Revision</label>
+					<select class="form-control" id="TipoRevision" onclick="MostrarTipoVacuna();">
+						<option>Castracion</option>
+						<option>Seguimiento</option>
+						<option>Vacuna</option>						
+					</select>
+				</div>
+				<div class="form-group" style="display: none;" id="divVacuna">
+					<label for="tipoVacuna">Tipo de vacuna</label>
+					<select class="form-control" id="tipoVacuna">
+						<option>-------	</option>
+						<option>Vacuna1</option>
+						<option>Vacuna2</option>
+						<option>Vacuna3</option>						
+					</select>
+				</div>
+				<div class="form-group" >
+					<label for="detalle">Detalle de Revision</label>
+					<textarea class="form-control"></textarea>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+				<button type="button" class="btn btn-primary">Registrar Revision</button>
+			</div>
+		</div>
+	</div>
 </div>
+
+
+<script type="text/javascript">
+
+	//--------Muestra u Oculta el input tipo vacuna------------//
+	function MostrarTipoVacuna() {
+		if ($('#TipoRevision').val()=='Vacuna') {
+			$('#divVacuna').show();
+		} else {
+			$('#divVacuna').hide();
+		}
+	}	
+</script>
