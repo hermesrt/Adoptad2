2662d32903196d317693b5e633bcc2fbdb1266ab
Hermes <hermesrt@gmail.com>
2018-10-03 15:43:03 -0300

    model usuario y otros

--- application/controllers/C_InicioSesion.php
+++ application/controllers/C_InicioSesion.php
@@ -3,12 +3,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class C_InicioSesion extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+ 		$this->load->model('M_Usuario');
+
+	}
+
 	public function index()
 	{
 		$this->load->view('Plantillas/V_Header');
 		$this->load->view('V_InicioSesion');
 		$this->load->view('Plantillas/V_Footer');
+		
 	}
+
+	public function InicioSesion(){
+
+		$this->form_validation->set_rules('usuario', 'usuario', 'required');
+		$this->form_validation->set_rules('contraseña', 'contraseña', 'required');
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->index();
+		}
+		else
+		{
+			$this->M_Usuario->IniciarSesion($this->input->post('usuario'), $this->input->post('contraseña'));
+			//var_dump($this->session->userdata());
+			#falta comprobar que el usuario y pass sean correctos
+			redirect('C_Inicio','refresh');
+		}
+	}
+
+
 
 }
 
--- application/views/V_InicioSesion.php
+++ application/views/V_InicioSesion.php
@@ -4,20 +4,21 @@
 		<div class="col">
 			<center>
 				<img src="<?= base_url('assets/img/logo.png') ?>">
-				<form>
-					<div class="form-group">
-						<label for="usuario">Usuario</label>
-						<input type="text" class="form-control" id="usuario" aria-describedby="emailHelp" placeholder="Ingrese usuario...">
-					</div>
-					<div class="form-group">
-						<label for="contraseña">Contraseña</label>
-						<input type="password" class="form-control" id="contraseña" placeholder="Ingrese contraseña...">
-					</div>
-					<button type="submit" class="btn btn-primary">iniciar Sesion</button>
-				</form>
+				<?php echo validation_errors(); ?>
+				<?php echo form_open('C_InicioSesion/InicioSesion'); ?>
+				<div class="form-group">
+					<label for="usuario">Usuario</label>
+					<input type="text" class="form-control" id="usuario" name="usuario" aria-describedby="emailHelp" placeholder="Ingrese usuario...">
+				</div>
+				<div class="form-group">
+					<label for="contraseña">Contraseña</label>
+					<input type="password" class="form-control" id="contraseña" name="contraseña" placeholder="Ingrese contraseña...">
+				</div>
+				<button type="submit" class="btn btn-primary">iniciar Sesion</button>
+			</form>
 
-			</center>
-		</div>
-		<div class="col"></div>
+		</center>
 	</div>
+	<div class="col"></div>
+</div>
 </div>
