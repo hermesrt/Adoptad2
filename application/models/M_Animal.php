e9ccd3de0a0fcac7ceeac4bb2996794fd6701c0f
lucas <lucasficus@gmail.com>
2018-10-04 17:32:45 -0300

    Añadi metodos a clase M_Animal
    Añadi metodos a la clase M_Animal, falto agregar la funcion agregarVacunaAnimal() para eso tenemos crear la clase Vacuna

--- application/models/M_Animal.php
+++ application/models/M_Animal.php
@@ -13,13 +13,16 @@ class M_Animal extends CI_Model {
     public $descripcion_animal;
     public $estado_animal;
     public $castrado;
+    public $adoptado;
     public $nombre_imagen_animal;
     public $id_centro;
-
+    public $revisiones;       //------->  es un array de muchos objetos revision    
+    public $vacunas;          //------->  es un array de muchos objetos vacuna
     
     //-------> iniciliza el objeto M_Animal con todos los valores de la columna que trae de la bd
     function init($row)
     {
+        $this -> load -> model('M_Revision','revision'); //---> cargo el modelo M_Revision 
         $this -> id_animal = $row -> id_animal;
         $this -> nombre_animal = $row -> nombre_animal;
         $this -> raza_animal = $row -> raza_animal;
@@ -27,8 +30,11 @@ class M_Animal extends CI_Model {
         $this -> sexo_animal = $row -> sexo_animal;
         $this -> edad_animal = $row -> edad_animal;
         $this -> castrado = $row -> castrado;
+        $this -> adoptado = $row -> adoptado;
         $this -> nombre_imagen_animal = $row -> nombre_imagen_animal;
         $this -> id_centro = $row -> id_centro;
+        //-----> obtengo todas las revisiones para un animal
+        $this -> revisiones = $this -> model -> revision -> obtenerRevisiones($this -> $id_animal);
     }
 
     
@@ -41,11 +47,10 @@ class M_Animal extends CI_Model {
         $query = $this->db->get();
 
         if ($query->num_rows() > 0) {
-            foreach ($query->result() as $row) {
-                $new_object = new self();
-                $new_object->init($row);
-                $result = $new_object;  //----> el resultado seria un array de objetos M_Animal
-            }
+            $row = $query->result();
+            $new_object = new self();
+            $new_object->init($row[0]);
+            $result = $new_object;  //----> el resultado seria un array de objetos M_Animal
             return $result;
         }else {
             return false;
@@ -71,16 +76,83 @@ class M_Animal extends CI_Model {
     }
     
     //--------- Recupera todos los animales por centro de adopcion
+    function obtenerPorCentro($id_centro)
+    {
+        $result = array();
+        $this->db->from("animal")->where('id_centro',$id_centro);
+        $query = $this->db->get();
+        if ($query->num_rows() > 0) {
+            foreach ($query->result() as $row) {
+                $new_object = new self();
+                $new_object->init($row);
+                $result[] = $new_object;  //----> el resultado seria un array de objetos M_Animal
+            }
+            return $result;
+        }else {
+            return false;
+        }
+    }
     
+    //----> Cambia el estado del animal, el estado es un string $estado
+    //----> $estado puede ser -> 'Activo','Inactivo',
+    function cambiarEstado($estado)
+    {
+        $this->db->set('estado_animal', $estado);
+        $this->db->where('id',$this -> id_animal);
+        $this->db->update('animal'); 
+    }
     
-    function cambiarEstado(){}
-    function agregarVacunaAnimal(){}
-    function estaCastrado(){}
-    function getEspecie(){}
-    function getSexo(){}
-    function getEdad(){}
-    function castrar(){}
-    function estaAdoptado(){}
+    function agregarVacunaAnimal()
+    {
+        
+    }
+    
+    //--> Cambia estado "castrado" de 0 a 1
+    //--> Si esta castrado = 1
+    //--> Si no esta castrado = 0
+    function estaCastrado()
+    {
+        if ($this -> castrado == 0) {
+            return false;   //--> castrado ==0 devuelve false
+        } else {
+            return true;   //--> castrado ==1 devuelve true
+        }
+    }
+    
+    function getEspecie()
+    {
+        return $this -> especie_animal;
+    }
+    
+    function getSexo()
+    {
+        return $this -> sexo_animal;
+    }
+    
+    function getEdad()
+    {
+        return $this -> edad_animal;
+    }
+    
+    //--> Cambia estado "castrado" del animal de 0 a 1
+    function castrar()
+    {
+        if ($this -> castrado == 0) {
+            $this->db->set('castrado',1);
+            $this->db->where('id',$this -> id_animal);
+            $this->db->update('animal'); 
+        }
+    }
+    
+    //--> Funcion que devuelve true o false si el animal esta adoptado o no esta adoptado
+    function estaAdoptado()
+    {
+        if ($this -> adoptado == 0) {
+            return false; //--> si adoptado == 0 no esta adoptado
+        } else {
+            return true;  //--> si adoptado == 1 esta adoptado
+        }
+    }
     
 }
 
